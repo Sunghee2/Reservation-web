6e5742c5ad0bd936de5a801b215d24fbe8641295
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2019-05-27 04:01:43 +0900

    Add reservation form

--- src/main/java/Controller/controller.java
+++ src/main/java/Controller/controller.java
@@ -1,13 +1,25 @@
 package Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class controller {
 
     @RequestMapping(value = "/")
-    public String test(){
+    public String index(){
         return "index";
     }
+
+//    컨트롤러 분리해야되는데 일단 냅둠
+    @RequestMapping(value = "/reservation/new")
+    public String reservation_new() {
+        return "classrooms/new";
+    }
+//    @RequestMapping("/form")
+//    public String form(Model model){
+//        model.addAttribute("user", new Reservation());
+//        return "form";
+//    }
 }
