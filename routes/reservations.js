1e92b49a4ae2adc1d2879ec8adcfa39a8e242df4
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2019-05-31 16:01:10 +0900

    Add ER_ACCESS_DENIED_ERROR handling

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql');
-const conn = mysql.createConnection(require('../config/db-config.js'));
-conn.connect()
+const conn = require('./db');
 
 router.get('/', (req, res, next) => {
     conn.query('SELECT * FROM reservation', (err, rows) => {
