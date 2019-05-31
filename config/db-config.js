155067e97731c47ec5f75d3fb7e095a625e3c60a
leean <leeeeean91@gmail.com>
2019-05-31 18:03:53 +0900

    Update db-config.js

--- config/db-config.js
+++ config/db-config.js
@@ -2,5 +2,6 @@ module.exports = {
     host: 'localhost',
     user: 'root',
     password: 'root', // 각자 비밀번호 입력하기
-    database: 'reservation'
+    database: 'reservation',
+    multipleStatements: true
 }
