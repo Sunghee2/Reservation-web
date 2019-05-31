fb157820a8e2fa80304c6ee7d68713449726d3e8
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2019-05-31 16:05:07 +0900

    Fix error message

--- routes/db.js
+++ routes/db.js
@@ -7,7 +7,7 @@ conn.connect(function(err) {
     if(err) {
         switch(err.code) {
             case 'ER_ACCESS_DENIED_ERROR':
-                throw new Error('config/db-config.js 에서 mysql 설정 해주세요.');
+                throw new Error('config/db-config.js 에서 mysql username, password, DB명 설정 해주세요.');
         }
     }
 });
